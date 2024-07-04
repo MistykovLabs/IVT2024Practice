b1897e9380e7a1ce8a2c710a40944d4578d2c868
Nikita Efimov <Nikita Efimov@DESKTOP-EFN3101>
2024-07-04 19:49:30 +0300

    Добавьте файлы проекта.

---
+++ IVT2024Pr2/header.h
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <windows.h>
+#include <clocale>
+
+// Version 2024.07.03
+
+using namespace std;
+
+// Базовый класс для генерации и проверки капчи
+class Captcha {
+public:
+    virtual string generateCaptcha() = 0;
+    virtual bool verifyCaptcha(const string& input) = 0;
+};
+
+// Класс для простой текстовой капчи
+class TextCaptcha : public Captcha {
+private:
+    string captchaText;
+
+public:
+    string generateCaptcha() override {
+        captchaText = generateRandomString(5);
+        return captchaText;
+    }
+
+    bool verifyCaptcha(const string& input) override {
+        return input == captchaText;
+    }
+
+private:
+    string generateRandomString(int length) {
+        string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        random_device rd;
+        mt19937 generator(rd());
+        uniform_int_distribution<> distribution(0, chars.size() - 1);
+
+        string randomString;
+        for (int i = 0; i < length; ++i) {
+            randomString += chars[distribution(generator)];
+        }
+        return randomString;
+    }
+};
+
+// Базовый класс для хранения учетных данных
+class Credentials {
+public:
+    virtual bool isValid() = 0;
+};
+
+// Класс для хранения логина и пароля
+class UserCredentials : public Credentials {
+private:
+    string login;
+    string password;
+
+public:
+    UserCredentials(const string& login, const string& password) : login(login), password(password) {}
+
+    bool isValid() override {
+        // Простая проверка: логин и пароль не пустые
+        return !login.empty() && !password.empty();
+    }
+
+    const string& getLogin() const { return login; }
+    const string& getPassword() const { return password; }
+};
+
+// Класс для взаимодействия с пользователем
+class UserInterface {
+public:
+    string requestInput(const string& message) {
+        cout << message;
+        string input;
+        cin >> input;
+        return input;
+    }
+
+    void showMessage(const string& message) {
+        cout << message << endl;
+    }
+};
+
+// Основной класс библиотеки с функциями
+class AuthenticationLibrary {
+private:
+    Captcha* captchaGenerator;
+    UserInterface* userInterface;
+
+public:
+    AuthenticationLibrary() {
+        captchaGenerator = new TextCaptcha();
+        userInterface = new UserInterface();
+    }
+
+    ~AuthenticationLibrary() {
+        delete captchaGenerator;
+        delete userInterface;
+    }
+
+    // Регистрация пользователя
+    UserCredentials* registration() {
+        cout << "Окно регистрации" << endl;
+        string login = userInterface->requestInput("Придумайте логин: ");
+        string password = userInterface->requestInput("Придумайте пароль: ");
+
+        UserCredentials* credentials = new UserCredentials(login, password);
+        if (credentials->isValid()) {
+            system("cls"); //1
+            userInterface->showMessage("Регистрация прошла успешно!");
+            return credentials;
+        }
+        else {
+            system("cls"); //1
+            userInterface->showMessage("Некорректные данные!");
+            delete credentials;
+            return nullptr;
+        }
+    }
+
+    // Авторизация пользователя
+    bool authorization(UserCredentials* credentials) {
+        cout << "Окно авторизации" << endl;
+        string login = userInterface->requestInput("Введите логин: ");
+        string password = userInterface->requestInput("Введите пароль: ");
+
+        if ((login == credentials->getLogin()) && (password == credentials->getPassword())) {
+            string captcha = captchaGenerator->generateCaptcha();
+            string captchaInput = userInterface->requestInput("Введите капчу (" + captcha + "): ");
+
+            if (captchaGenerator->verifyCaptcha(captchaInput)) {
+                userInterface->showMessage("Авторизация успешна!");
+                return true;
+            }
+            else {
+                userInterface->showMessage("Неверная капча!");
+            }
+        }
+        else {
+            userInterface->showMessage("Неверный логин или пароль!");
+        }
+        return false;
+    }
+
+    // Запрос произвольных параметров (пример)
+    string requestOtherParameters() {
+        return userInterface->requestInput("Введите произвольный параметр: ");
+    }
+};
---
+++ IVT2024Pr2/main.cpp
@@ -0,0 +1,57 @@
+#include "header.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+
+    AuthenticationLibrary authLib;
+
+    // Регистрация пользователя
+    UserCredentials* user = authLib.registration();
+
+    // Авторизация пользователя и функции доступные только при успешной авторизации
+    if (user != nullptr && authLib.authorization(user)) {
+
+        // Меню открывается после авторизации
+        int choice;
+        do {
+            cout << "\n";
+            cout << "Меню: \n";
+            cout << "1. Ввод произвольного параметра \n";
+            cout << "2. Получение параметров системы \n";
+            cout << "3. Выход \n";
+            cout << "Введите свой выбор: ";
+            cin >> choice;
+            cout << "\n";
+
+            switch (choice) 
+            {
+                case 1: {
+                    // Установка произвольного параметра
+                    string otherParam = authLib.requestOtherParameters();
+                    cout << "Получен произвольный параметр: " << otherParam << endl;
+                    break;
+                }
+                case 2: {
+                    // Получение параметров системы
+                    cout << std::system("systeminfo") << endl;
+                    break;
+                }
+                case 3: {
+                    // Выход
+                    cout << "Выход... \n";
+                    delete user;
+                    break;
+                }
+                default:
+                    cout << "Неверный выбор. \n";
+                    break;
+            }  
+        } while (choice != 3);
+    }
+
+    return 0;
+}
